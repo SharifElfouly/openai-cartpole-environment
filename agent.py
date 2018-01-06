77504b675d950236d9852f097e5bdb7cf7a17f30
Sharif Elfouly <selfouly@gmail.com>
2018-01-06 20:42:02 +0100

    made program dynamic

--- agent.py
+++ agent.py
@@ -8,15 +8,16 @@ from collections import Counter
 LR = 1e-3
 env = gym.make('CartPole-v0')
 env.reset()
-goal_steps = 500
-score_requirement = 50
-initial_games = 10000
+GOAL_STEPS = 500
+SCORE_REQUIREMENT = 50
+INITIAL_GAMES = 10000
+GAMES_TO_PLAY = 5
 
 
 def some_random_games_first():
     for episode in range(5):
         env.reset()
-        for t in range(goal_steps):
+        for t in range(GOAL_STEPS):
             env.render()
             action = env.action_space.sample()
             observation, reward, done, info = env.step(action)
@@ -28,11 +29,11 @@ def initial_population():
     training_data = []
     scores = []
     accepted_scores = []
-    for _ in range(initial_games):
+    for _ in range(INITIAL_GAMES):
         score = 0
         game_memory = []
         prev_observation = []
-        for _ in range(goal_steps):
+        for _ in range(GOAL_STEPS):
             action = env.action_space.sample()
             observation, reward, done, info = env.step(action)
 
@@ -44,7 +45,7 @@ def initial_population():
             if done:
                 break
 
-        if score >= score_requirement:
+        if score >= SCORE_REQUIREMENT:
             accepted_scores.append(score)
             for data in game_memory:
                 if data[1] == 1:
@@ -100,14 +101,14 @@ def create_one_hot(y):
 
 def play(model):
     scores = []
-    for episode in range(100):
+    for episode in range(GAMES_TO_PLAY):
         env.reset()
         action = env.action_space.sample()
         score = 0
-        for t in range(goal_steps):
+        for t in range(GOAL_STEPS):
             env.render()
             observation, reward, done, info = env.step(action)
-            observation = np.array(observation).reshape(1, 4)
+            observation = np.array(observation).reshape(1, len(observation))
             action = np.argmax(model.predict(observation))
             score += reward
             if done:
@@ -116,15 +117,14 @@ def play(model):
     print('Average score: {}'.format(sum(scores)/len(scores)))
 
 
-model = create_model(4, 2)
-data = initial_population()
-x = np.array([i[0] for i in data])
-y = np.array([i[1] for i in data])
-labels = create_one_hot(y)
-model = train_model(model, x, labels)
-play(model)
-
-
-
+if __name__ == '__main__':
+    data = initial_population()
+    x = np.array([i[0] for i in data])
+    y = np.array([i[1] for i in data])
+    n_labels = len(np.unique(y))
+    model = create_model(len(data[0][0]), n_labels)
+    labels = create_one_hot(y)
+    model = train_model(model, x, labels)
+    play(model)
 
 
